d337f0b57f76706d99adf91a5216b792e3a45e2c
Kulanthaivel <AE10195@ad.wellpoint.com>
2020-08-13 09:58:23 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { StarterHomeComponent } from './starter-home/starter-home.component';
 
-
+const routes: Routes = [
+  {path: '', redirectTo: '/pharmacist', pathMatch: 'full' },
+  {path: 'pharmacist' , component: StarterHomeComponent},
+  {path: '**', redirectTo: '/pharmacist', pathMatch: 'full' }
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
